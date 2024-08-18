b19a837c40ec04854993b5b44726ddb81fe37768
contributorsambhav <sambhav511974@gmail.com>
2024-08-18 21:49:03 +0530

    ..

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: '*:*',
+  origin: 'https://classroom-4v2s.onrender.com',
   methods : ["GET","POST","PUT","PATCH","DELETE"],
   credentials : true
 
